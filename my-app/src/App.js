e4c4239f5a1f54a83b30b1f947759128d619edbe
leejaewon <leejaewon6463@icloud.com>
2024-11-21 01:23:11 +0900

    Add website content

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My Website</h1>
+      <p>This is a React app hosted on GitHub Pages!</p>
+    </div>
+  );
+}
+
+export default App;
